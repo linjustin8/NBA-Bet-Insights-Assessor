840f60c88dda725a75e8873baea5477f8320b7c4
justin <justinlin@ufl.edu>
2024-04-21 07:12:26 -0400

    first commit

---
+++ src/headshot.py
@@ -0,0 +1,10 @@
+#headshot.py
+import requests
+
+class Headshots:
+    def __init__(self, player):
+        self.player = player
+        self.playerID = self.getPlayerID(self.player)
+        
+    def getPlayerID(self, player):
+        pass
---
+++ src/main.py
@@ -0,0 +1,11 @@
+#main.py
+from tkinter import *
+root = Tk()
+root.title("Goat Gambler")
+root.geometry("800x800")
+
+def main():
+    balls = "balls"
+    root.mainloop()
+    
+main()
