1557628f55d46c64b5862c307f57d6e89daa1faa
justin <justinlin@ufl.edu>
2024-04-22 15:39:22 -0400

    finished basic structure of results page

--- src/results.py
+++ src/results.py
@@ -1,45 +1,73 @@
 # results.py
 from tkinter import *
 from customtkinter import *
+from PIL import Image, ImageTk
 from headshot import Headshots
 
+
 class Results:
-    def __init__(self, player, points):
+    def __init__(self, player, overUnder):
         self.player = player
-        self.points = points
+        self.overUnder = overUnder
         
     def getPercentage(self):
         pass
         
+    def adjustFontSize(self, label):
+        pass
+    
+    def handleVisualizer(self):
+        pass
+    
     def displayResults(self, inputPage):
         resultsPage = Toplevel(inputPage)
         resultsPage.title("Goat Gambler")
-        resultsPage.geometry("600x800")
+        resultsPage.geometry("800x1000")
         
-        mainframe = Frame(resultsPage, width=590, height=790, highlightbackground="#6B6B6B", highlightthickness=8)
+        mainframe = Frame(resultsPage, width=790, height=990, highlightbackground="#6B6B6B", highlightthickness=8)
         mainframe.grid(row=0, column=0, sticky="nswe")
         resultsPage.columnconfigure(0, weight=1)
         resultsPage.rowconfigure(0, weight=1)
         
         # player title label
-        title = CTkLabel(mainframe, text="PLAYER:", font=("Lucida Console", 48, "bold", "underlined"), text_color="black")
-        title.grid(row=1, column=1)
+        title = CTkLabel(mainframe, text="PLAYER", font=("Lucida Console", 42, "bold", "underline"), text_color="#A0A0A0")
+        title.grid(row=0, column=0, sticky="s", padx=10, pady=10)
         
         # player name label
-        
+        player = CTkLabel(mainframe, text="Jaime Jaquez Jr.", font=("Lucida Console", 42, "bold"), text_color="black")
+        player.grid(row=1, column=0, sticky="n")
         
         # over/under input label
+        ouText = CTkLabel(mainframe, text="OVER/UNDER(pts):", font=("Lucida Console", 32, "bold"), text_color="black")
+        ouText.grid(row=8, column=0, sticky="w", padx=10)
+        ouInput = CTkLabel(mainframe, text="8.5", font=("Lucida Console", 32, "bold"), text_color="black")
+        ouInput.grid(row=8, column=0, sticky="e", padx=10)
+    
+        # playerImage
+        imageFile = Image.open("assets/Derrick_White.png")
+        imageFile = imageFile.resize((624, 456), Image.LANCZOS)
+        tkImage = ImageTk.PhotoImage(imageFile)
+        
+        playerHeadshot = CTkLabel(mainframe, image=tkImage, text="")
+        playerHeadshot.image = tkImage # keeping a reference of the image
+        playerHeadshot.grid(row=3, column=0)
         
         
         # odds(percentage) label
-        
+        percentText = CTkLabel(mainframe, text="ODDS:", font=("Lucida Console", 32, "bold"), text_color="black")
+        percentText.grid(row=9, column=0, sticky="w", padx=10)
+        percentOutput = CTkLabel(mainframe, text="33.91%", font=("Lucida Console", 32, "bold"), text_color="black")
+        percentOutput.grid(row=9, column=0, sticky="e", padx=10)
         
         # open visualizer button
+        openVisualizer = CTkButton(mainframe, fg_color="#A0A0A0", text="OPEN DATA VISUALIZER", hover_color="#4C4C4C",
+                         font=("Lucida Console", 32, "bold"),corner_radius=25, height=50, width=400,
+                         command=self.handleVisualizer(), text_color="#282828")
+        openVisualizer.grid(row=13, column=0, padx=10)
         
         
-        
-        mainframe.columnconfigure(0,1,2, weight=1)
-        mainframe.rowconfigure((0,1,2,4,5,6,7), weight=1)
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure((0,1,2,4,5,6,7,8,9,10,11,12,13,14,15), weight=1)
         
         resultsPage.grab_set()
         resultsPage.transient(inputPage)
--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,5 +1,14 @@
-from headshot import Headshots
+import tkinter as tk
 
-x = Headshots("Derrick White")
+root = tk.Tk()
+root.geometry("300x300")
 
-x.downloadImage()
+# Configuring row weights
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=3)
+
+# Widget with specific vertical alignment and expansion
+label = tk.Label(root, text="Flexible Vertical Placement", bg="khaki")
+label.grid(row=0, column=0, sticky="NS", rowspan=2)
+
+root.mainloop()
