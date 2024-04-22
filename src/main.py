b8d036c036747d5e1afdb4c6261e072515fa5f3a
justin <justinlin@ufl.edu>
2024-04-21 21:30:49 -0400

    gave variables proper names and added check pts and player functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #main.py
 from tkinter import *
-from tkinter import ttk
 from customtkinter import *
 from heap import *
 
@@ -37,28 +36,28 @@ def main():
     player.insert(1.0, "Player Name...")
     player.bind("<FocusIn>", lambda event, p=player: playerFocused(event, p))
     player.bind("<FocusOut>", lambda event, p=player: playerUnfocused(event, p))
-    player.grid(row=2, column=0)  
+    player.grid(row=3, column=0)  
     print(player.cget("text_color"))
 
     #input widget for point value
     pts = CTkTextbox(mainframe, fg_color="#A0A0A0", font=("Lucida Console", 25), corner_radius=25, height=40, width=400)
     pts.insert(1.0, "Pts Over/Under...")
-    pts.grid(row=3, column=0)  
+    pts.grid(row=4, column=0)  
     pts.bind("<FocusIn>", lambda event, p=pts: ptsFocused(event, p))
     pts.bind("<FocusOut>", lambda event, p=pts: ptsUnfocused(event, p))
     player.bind("<Return>", lambda event, p=pts: handleEnter(event, p)) 
     pts.bind("<Return>", lambda event, p=pts: handleEnter(event, p)) 
     
     #finalize inputs button
-    detonate = CTkButton(mainframe,fg_color="#A0A0A0", text="Finalize Selection",
+    getResults = CTkButton(mainframe,fg_color="#A0A0A0", text="Finalize Selection",
                          font=("Lucida Console", 25),corner_radius=25, height=40, width=400,
-                         command=lambda: check_input(player,pts))
-    detonate.grid(row=4,column=0)
+                         command=lambda: checkInput(player.get(1.0, "end-1c"), pts.get(1.0, "end-1c")))
+    getResults.grid(row=6,column=0, sticky="n")
 
 
 
     mainframe.columnconfigure(0, weight=1)
-    mainframe.rowconfigure((0,1,2,4,5,6), weight=1)
+    mainframe.rowconfigure((0,1,2,4,5,6,7), weight=1)
 
 
     root.mainloop()
@@ -72,43 +71,49 @@ def handleEnter(event, pts):
 def ptsFocused(event, textbox):
     if textbox.get(1.0,"end-1c") == "Pts Over/Under...":
         textbox.delete(1.0,"end-1c")
-        textbox.configure(fg_color="#6B6B6B")
-        textbox.configure(text_color="#FFFFFF")
+        textbox.configure(fg_color="#6B6B6B", text_color="#FFFFFF")
 
 def ptsUnfocused(event, textbox):
     if textbox.get(1.0,"end-1c") == "":
        textbox.insert(1.0, "Pts Over/Under...")
-       textbox.configure(fg_color="#A0A0A0")
-       textbox.configure(text_color="#DCE4EE")
+       textbox.configure(fg_color="#A0A0A0", text_color="#DCE4EE")
        
 def playerFocused(event, textbox):
     if textbox.get(1.0,"end-1c") == "Player Name...":
         textbox.delete(1.0,"end-1c") 
-        textbox.configure(fg_color="#6B6B6B")
-        textbox.configure(text_color="#FFFFFF")
+        textbox.configure(fg_color="#6B6B6B", text_color="#FFFFFF")
 
 def playerUnfocused(event, textbox):
     if textbox.get(1.0,"end-1c") == "":
        textbox.insert(1.0, "Player Name...")
-       textbox.configure(fg_color="#A0A0A0")
-       textbox.configure(text_color="#DCE4EE")
+       textbox.configure(fg_color="#A0A0A0", text_color="#DCE4EE")
 
 #Jimy don't touch the check_input or anything related to that, I got it
 #I need you to create a second screen where we display information if input passes check
-def check_input(player, pts):
-    df = dataframe_init()
-    while (True):
-        if (checkPts(pts) and checkName(player)):
-            return True
-        else :
-
-            continue
+def checkInput(player, pts):
+    # df = dataframe_init()
+    if (checkPts(pts) and checkName(player)):
+        print("WORKING")
+        
+    else :
+        print("FAILED")
+        
 
 def checkPts(points):
-    pass
+    for char in points:
+        if (not(ord(char) > 47 and ord(char) < 58)):
+            print("not work: pts!")
+            return False    
+    return True
 
 def checkName(player):
-    pass
+    for char in player:
+        if (not(ord(char) > 96 and ord(char) < 123) and (ord(char) != 32) 
+            and (ord(char) != 45) and (ord(char) != 39)):
+            print("not work: name!")
+            return False
+    #also will need to implement a check to see if player exists in database
+    return True
 
 
 if __name__ == "__main__":
