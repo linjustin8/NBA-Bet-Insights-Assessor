15aec314425c0d1c20f8d13f99dc2de14f3437fe
ethan <ethan.lee.zhu@gmail.com>
2024-04-22 11:39:13 -0400

    working on adding player reference ID to existing df

--- src/heap.py
+++ src/heap.py
@@ -7,6 +7,7 @@ import heapq
 #default initiator to grab information from csv and organize it into a map
 def dataframe_init():
     filename = '../assets/boxscore_scrape.csv'
+    filename_head = '../assets/NBA_Player_IDs.csv'
 
     originaldf = pd.read_csv(filename) #maintain original dataframe
     modifieddf=originaldf.copy() #modifying new dataframe
@@ -22,11 +23,30 @@ def dataframe_init():
     #organize csv by player name, and organize each player by points
     modifieddf=modifieddf.sort_values(by=['playerName','PTS'],ascending=[True,False])
 
-    print(originaldf.head())
-    print(modifieddf.head())
+    #new dataframe, following ISO-8859-1 update
+    player_head = pd.read_csv(filename_head,encoding='ISO-8859-1')
+    player_head = player_head.iloc[:,[0,2]]
 
-    return modifieddf
+    originaldf = (final_df_frame(modifieddf,player_head))
 
+    print(originaldf)
+
+    return originaldf
+
+
+def final_df_frame(df1,df2):
+
+    #create local dataframe to be returned, and create new column with all rows NaN
+    df_base = df1
+    df_base['BBRefID']=np.nan
+
+    #creating a dictionary by zipping two columns and assinging key-value pairs
+    dictionary_of_IDs = dict(zip(df2['BBRefName'],df2['BBRefID']))
+    for (key,value) in dictionary_of_IDs.items():
+
+
+
+    return (df_base)
 
 
 def list_o_point_create(df):
